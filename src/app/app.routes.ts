521589ca395f27010ae6d1e0c053b3b3bdd7ee51
lukasbuschdev <buschlukas175@gmail.com>
2025-10-04 22:50:56 -0600

    **updated**: deleted unnecesary code and replaced img `close.svg` to uptimize performance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
-import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
-import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'cli', pathMatch: 'full' },
-    { path: 'privacy-policy', component: PrivacyPolicyComponent },
-    { path: 'legal-notice', component: LegalNoticeComponent }
+    { path: '', redirectTo: 'cli', pathMatch: 'full' }
 ];
